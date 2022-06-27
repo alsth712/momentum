6fdcc0ddb7738d5704c6075e4eceac447605e644
alsth712 <alsth712@naver.com>
2022-06-27 18:05:15 +0900

    🐛 Fixing a bug

--- MOMENTUM/js/quotes.js
+++ MOMENTUM/js/quotes.js
@@ -1,46 +1,50 @@
 const quotes = [
   {
-    quote:"The greatest glory in living lies not in never falling, but in rising every time we fall.",
-    author:"Nelson Mandela",
+    quote:
+      "The greatest glory in living lies not in never falling, but in rising every time we fall.",
+    author: "Nelson Mandela",
   },
   {
-    quote:"The way to get started is to quit talking and begin doing.",
-    author:"Walt Disney",
+    quote: "The way to get started is to quit talking and begin doing.",
+    author: "Walt Disney",
   },
   {
-    quote:"Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
-    author:"Steve Jobs",
+    quote:
+      "Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking.",
+    author: "Steve Jobs",
   },
   {
-    quote:"If life were predictable it would cease to be life, and be without flavor.",
-    author:"Eleanor Roosevelt",
+    quote:
+      "If life were predictable it would cease to be life, and be without flavor.",
+    author: "Eleanor Roosevelt",
   },
   {
-    quote:"If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
-    author:"Oprah Winfrey",
+    quote:
+      "If you look at what you have in life, you'll always have more. If you look at what you don't have in life, you'll never have enough.",
+    author: "Oprah Winfrey",
   },
   {
-    quote:"If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
-    author:"James Cameron",
+    quote:
+      "If you set your goals ridiculously high and it's a failure, you will fail above everyone else's success.",
+    author: "James Cameron",
   },
   {
-    quote:"Life is what happens when you're busy making other plans.",
-    author:"John Lennon",
+    quote: "Life is what happens when you're busy making other plans.",
+    author: "John Lennon",
   },
   {
-    quote:"Never let the fear of striking out keep you from playing the game.",
-    author:"Babe Ruth",
+    quote: "Never let the fear of striking out keep you from playing the game.",
+    author: "Babe Ruth",
   },
   {
-    quote:"Believe you can and you're halfway there.",
-    author:"Theodore Roosevelt",
+    quote: "Believe you can and you're halfway there.",
+    author: "Theodore Roosevelt",
   },
   {
-    quote:"Everything you've ever wanted is on the other side of fear.",
-    author:"George Addair",
+    quote: "Everything you've ever wanted is on the other side of fear.",
+    author: "George Addair",
   },
 ];
-
 
 const quote = document.querySelector("#quote span:first-child");
 const author = document.querySelector("#quote span:last-child");
@@ -48,4 +52,4 @@ const author = document.querySelector("#quote span:last-child");
 const todaysQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
 quote.innerText = todaysQuote.quote;
-author.innerText = `- todaysQuote.author -`;
+author.innerText = `- ${todaysQuote.author} -`;

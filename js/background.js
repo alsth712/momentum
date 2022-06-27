e68b9c39cdf2a7242afafc58e98f93c4e0c3779e
alsth712 <alsth712@naver.com>
2022-06-27 18:11:06 +0900

    🚚 Change file path

---
+++ js/background.js
@@ -0,0 +1,10 @@
+const images = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.png", "9.png", "10.png", "11.png" ];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+
+bgImage.src = `img/${chosenImage}`;
+bgImage.id = "bg-image";
+
+document.body.style = `background-image: url("${bgImage.src}")`;
